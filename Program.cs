d69eec39a3a0bb2cfe1b3ff4141a7dfd2d540d49
LeeJeongbin <ljb97310@naver.com>
2024-08-19 15:03:23 +0900

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleMiniGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(); // 게임 인스턴스 생성
+            game.Start(); // 게임 스투알..ㅌ
+        }
+    }
+}
